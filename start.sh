96f18eebce603ea6261c30564e9afc1891be3e9e
zzwwsy <1243983018@qq.com>
2019-06-29 00:31:28 +0800

    更改sh文件

--- start.sh
+++ start.sh
@@ -8,6 +8,3 @@ mvn spring-boot:start
 cd ../subscribe
 mvn spring-boot:start
 
-cd ../unsubscribe
-mvn spring-boot:start
-
--- stop.sh
+++ stop.sh
@@ -7,6 +7,3 @@ mvn spring-boot:stop
 cd ../subscribe
 mvn spring-boot:stop
 
-cd ../unsubscribe
-mvn spring-boot:stop
-
